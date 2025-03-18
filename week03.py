5f49070074cb6c19adb63f18f3a911b36c6a1291
SangIn-Lee <silee810@naver.com>
2025-03-18 18:41:33 +0900

    week03_1

--- week03.py
+++ week03.py
@@ -1,11 +1,14 @@
 # 1) Ice Americano : 2000    2) Cafe Latte : 3000
 drinks = ["Ice Americano", "Cafe Latte", "Watermelon Juice"]
 prices = [2000, 3000]
-amount = [0, 0]
-total_price = 0
-# order_list = ''
+amount = [0, 0, 0]
+
+menu_lists = ""
+for k in range(len(drinks)):
+    menu_lists = menu_lists + f"{k+1}) {drinks[k]} {prices[k]}won   "
+menu_lists = menu_lists + f"{len(drinks)+1}"
 while True:
-    menu = input(f"1) {drinks[0]} {prices[0]}won  2) {drinks[1]} {prices[1]}  3) {drinks[2]} {prices[2]}")
+    menu = input(menu_lists)
     if menu == "1":
         print(f"{drinks[0]} ordered. Price : {prices[0]}won")
         total_price = total_price + prices[0]
@@ -18,12 +21,12 @@ while True:
         print(f"{drinks[2]} ordered. Price : {prices[2]}won")
         total_price = total_price + prices[2]
         amounts[2] = amount[2] + 1
+    elif menu == "4":
+        print("Finish order~")
+    else:
         break
-
-#print(f"{drinks[0]} {prices[0]} x{amounts[0]} {prices[0]*amounts[0]}")
-#print(f"{drinks[1]} {prices[1]} x{amounts[1]} {prices[1]*amounts[1]}")
-#print(f"{drinks[2]} {prices[2]} x{amounts[2]} {prices[2]*amounts[2]}")
 print ("Product   Price   Amount  Subtotal")
 for i in range(len(drinks)):
+    if amounts[i] > 0:
     print(f"{drinks[i]} {prices[i]} x{amounts[i]} {prices[i] * amounts[i]}")
 print(f"Total price : {total_price}")

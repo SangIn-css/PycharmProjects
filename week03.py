b4e0913f3d012f79bdfa714a134d95ec7ee2834c
SangIn-Lee <silee810@naver.com>
2025-03-18 19:12:59 +0900

    week03_1

--- week03.py
+++ week03.py
@@ -1,36 +1,37 @@
-# 1) Ice Americano : 2000    2) Cafe Latte : 3000
 drinks = ["Ice Americano", "Cafe Latte", "Watermelon Juice"]
-prices = [2000, 3000]
-amount = [0, 0, 0]
+prices = [2000, 3000, 4900]
+amounts = [0, 0, 0]
 total_price = 0
 
+# drinks = ["Ice Americano", "Cafe Latte"]
+# prices = [2000, 3000]
+# amounts = [0, 0]
+# total_price = 0
+
+def order_process(idx: int):
+    global total_price
+    print(f"{drinks[idx]} ordered. Price : {prices[idx]}won")
+    total_price = total_price + prices[idx]
+    amounts[idx] = amounts[idx] + 1
 
 
 menu_lists = ""
 for k in range(len(drinks)):
-    menu_lists = menu_lists + f"{k+1}) {drinks[k]} {prices[k]}won   "
-menu_lists = menu_lists + f"{len(drinks)+1}"
+    menu_lists = menu_lists + f"{k+1}) {drinks[k]} {prices[k]}won  "
+menu_lists = menu_lists + f"{len(drinks)+1}) Exit : "
+
 while True:
     menu = int(input(menu_lists))
-    if len(drinks)>= menu >= 1:
+    if len(drinks) >= menu >= 1:
         order_process(menu - 1)
-        print(f"{drinks[0]} ordered. Price : {prices[0]}won")
-        total_price = total_price + prices[0]
-        amounts[0] = amount[0] + 1
-    elif menu == "2":
-        print(f"{drinks[1]} ordered. Price : {prices[1]}won")
-        total_price = total_price + prices[1]
-        amounts[1] = amount[1] + 1
-    elif menu == "3":
-        print(f"{drinks[2]} ordered. Price : {prices[2]}won")
-        total_price = total_price + prices[2]
-        amounts[2] = amount[2] + 1
-    elif menu == "4":
+    elif menu == len(drinks)+1:
         print("Finish order~")
-    else:
         break
-print ("Product   Price   Amount  Subtotal")
+    else:
+        print(f"{menu} menu is not exist. please choose from above menu.")
+
+print("Product  Price  Amount  Subtotal")
 for i in range(len(drinks)):
     if amounts[i] > 0:
-    print(f"{drinks[i]} {prices[i]} x{amounts[i]} {prices[i] * amounts[i]}")
+        print(f"{drinks[i]} {prices[i]} x{amounts[i]} {prices[i] * amounts[i]}")
 print(f"Total price : {total_price}")

96dafc44c33def3e0d143efade10c991eb9e345e
SangIn-Lee <silee810@naver.com>
2025-04-08 19:47:52 +0900

    week06 v2.8

--- kiosk.py
+++ kiosk.py
@@ -123,8 +123,11 @@ class OrderProcessor:
         Function that Produce next ticket number
         :return: next ticket number
         """
-        with open("ticket_number.txt", "r") as fp:
-            number = int (fp.read())
+        try:
+            with open("ticket_number.txt", "r") as fp:
+                number = int (fp.read())
+        except  FileNotFoundError:
+                number = 0
         number = number + 1
 
         with open("ticket_number.txt", "w") as fp:

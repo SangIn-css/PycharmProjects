aaf647ebb510b87af29d637667b6b0958376ee86
SangIn-Lee <silee810@naver.com>
2025-04-08 18:38:05 +0900

    week06 kiosk v2.5

--- kiosk.py
+++ kiosk.py
@@ -135,12 +135,3 @@ class OrderProcessor:
                 print(e)  # Display the specific IndexError message
 
         self.print_receipt(menu)
-
-
-if __name__ == "__main__":
-    menu_drinks = ["Ice Americano", "Cafe Latte", "Watermelon Juice"]
-    menu_prices = [2000, 3000, 4900]
-
-    menu = Menu(menu_drinks, menu_prices)
-    order_processor = OrderProcessor() # Menu 객체를 생성하지 않음
-    order_processor.run(menu) # run 메서드에 Menu 객체를 전달
